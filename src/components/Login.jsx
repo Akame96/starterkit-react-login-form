d17af655f14e7df887bb4e1ab67a465025fd57f3
Nunzia Biele <nunzia.biele@virgilio.it>
2024-09-30 17:12:46 +0200

    login form

---
+++ src/components/Login.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full">
+        <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div>
+            <label className="block text-sm font-medium text-gray-700">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="inserire email"
+            />
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              placeholder="inserire password"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+
+        </div>
+          <button
+            type="submit"
+            className="w-full py-2 px-4 bg-green-500 text-white font-semibold rounded-md shadow-sm hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+          >
+            Accedi
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// src/main.jsx o src/main.js
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'  // Importa il file CSS con Tailwind
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
